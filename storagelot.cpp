68ae935e0a683e8ba4a348d3774df4672e5e1f7a
metta rahul raj <142306268+rahulraj4404@users.noreply.github.com>
2024-08-21 12:26:51 +0530

    Update storagelot.cpp

--- storagelot.cpp
+++ storagelot.cpp
@@ -1,64 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class storage
+class store 
 {
     public:
-        int n,m,noofdays;
-        vector<vector<int>>grid;
-        grid.resize(n,(m,0));
-        vector<vector<int>>profit(noofdays);
-    
-    
-    bool isspaceavailable(int length,int width,int days)
+        int length, width, noofdays, rent, totalprofit;
+        int area;
+        vector<int> profit;
+    store(int l, int w) : length(l), width(w) 
     {
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;i++)
-            {
-                if(grid[i][j]==0 && grid[i+length][j+width]==0)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
+        area = length * width;
+        totalprofit = 0;
     }
-    bool checkprofit(int days,int priceperday)
+    bool acceptbox(int length, int width, int noofdays, int rent) 
     {
-        int totalprofit=0,profitforxdays=0;
-        for(int p:profit)
+        int areaofnewone = length * width;
+        if (areaofnewone > area)
+            return false;
+        int profitforxdays = checkprofit(length, width, noofdays, rent);
+        if (profitforxdays == 0)
+            return false;
+        else 
         {
-            totalprofit+=p;
-        }
-        int profitperday=totalprofit/noofdays;
-        profitforxdays=priceperday*days;
-        if(profitforxdays>=profitperday)
+            area -= areaofnewone;
+            cout << "Profit for no of days: " << profitforxdays << endl;
+            totalprofit = accumulate(profit.begin(), profit.end(), 0);
+            cout << "Total profit: " << totalprofit << endl;
             return true;
+        }
+    }
+    int checkprofit(int length, int width, int noofdays, int rent) \
+    {
+        int last30days = profitfor30days(min(noofdays, 30));  
+        int newprofit = noofdays * rent;
+        if (newprofit >= last30days)
+        {
+            for (int i = 0; i < noofdays; ++i)
+                profit.push_back(rent);
+            return newprofit;
         else
-            return false;
-        
+            return 0;
     }
-    bool cantakebox(int length,int width,int days,int priceperday)
+    int profitfor30days(int noofdays) 
     {
-        if(!isspaceavailable(int length,int width))
-            return false;
-        if(!checkprofit(int days,int priceperday))
-            return false;
-    }
-    int profitmade(int noofdays,int priceperday)
-    {
-        int totalprofitmade=priceperday*noofdays; // for a specific box 
-        return totalprofitmade;
-    }
-    int totalprofit()
-    {
-        int overallprofit=0;
-        for(int p:profit)
-            overallprofit+=p;
-        return overallprofit;   // overall profit
+        int totalprofit = 0;
+        int daysToConsider = min(noofdays, (int)profit.size());
+        for (int i = 0; i < daysToConsider; ++i)
+            totalprofit += profit[i];
+        return totalprofit;
     }
 };
-int main()
-{
-    
+int main() {
+    store myStore(10, 10);
+    bool item1 = myStore.acceptbox(2, 2, 4, 10);
+    if (!item1)
+        cout << "Cannot store box" << '\n';
+    return 0;
 }
